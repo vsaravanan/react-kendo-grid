1c347372f25a252b096b5bcc856f1d34bbc998a8
Viswanathan Saravanan <saravanan.resume@gmail.com>
2018-06-16 19:07:38 +0800

    upgrade to version 1

--- src/index.js
+++ src/index.js
@@ -1,63 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 
-import { orderBy } from '@progress/kendo-data-query';
+import { orderBy } from "@progress/kendo-data-query";
 
-import { products as productimp } from './products.json';
+import { products as productimp } from "./products.json";
 
 const products = productimp.data;
 
 class App extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            fields: Object.getOwnPropertyNames(products[0]).slice(0, 5),
-            products: this.GetProducts([]),
-            sort: [],
-            //allowUnsort: true,
-           // multiple: false
-        };
-        
-        this.sortChange = this.sortChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: this.GetProducts([]),
+      sort: []
+      //     this.GetProducts([]);
 
-    sortChange(event) {
-        this.setState({
-            products: this.GetProducts(event.sort),
-            sort: event.sort
-        });
-    }
+      //allowUnsort: true,
+      // multiple: false
+    };
+    this.sortChange = this.sortChange.bind(this);
+  }
 
-    GetProducts(sort) {
-        return orderBy(products, sort);
-    }
+  componentDidMount() {
+    // this.GetProducts([]);
+  }
 
-    render() {
-        
-        return (
-            <div>
+  sortChange(event) {
+    this.setState({
+      products: this.GetProducts(event.sort),
+      sort: event.sort
+    });
+  }
 
-                <Grid
-                    style={{ height: '300px' }}
-                    data={this.state.products}
-                    sortable={true}
-                        //allowUnsort: true,
-                        //mode: this.state.multiple ? 'multiple' : 'single'
-                    
-                    sort={this.state.sort}
-                    onSortChange={this.sortChange}
-                >
-                    <Column field="ProductID" />
-                    <Column field="ProductName" title="Product Name" />
-                    <Column field="UnitPrice" title="Unit Price" />
-                </Grid>
-            </div>
-        );
-    }
+  GetProducts(sort) {
+    return orderBy(products, sort);
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid
+          style={{ height: "300px" }}
+          data={this.state.products}
+          sortable={true}
+          //allowUnsort: true,
+          //mode: this.state.multiple ? 'multiple' : 'single'
+
+          sort={this.state.sort}
+          onSortChange={this.sortChange}
+        >
+          <Column field="ProductID" />
+          <Column field="ProductName" title="Product Name" />
+          <Column field="UnitPrice" title="Unit Price" />
+        </Grid>
+      </div>
+    );
+  }
 }
-
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
